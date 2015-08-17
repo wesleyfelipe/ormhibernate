808108043d4f951ce0dc35562ea26a3aeee78ea6
Wesley <wesleys.felipe@gmail.com>
2015-08-16 21:37:19 -0300

    Classe Departamento

--- src/main/java/com/unisinos/bancodedados2/ga/ormhibernate/app/GerenciadorBaseDeDados.java
+++ src/main/java/com/unisinos/bancodedados2/ga/ormhibernate/app/GerenciadorBaseDeDados.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import com.unisinos.bancodedados2.ga.ormhibernate.model.Cor;
+import com.unisinos.bancodedados2.ga.ormhibernate.model.Departamento;
 import com.unisinos.bancodedados2.ga.ormhibernate.model.Empresa;
 import com.unisinos.bancodedados2.ga.ormhibernate.model.Holding;
 import com.unisinos.bancodedados2.ga.ormhibernate.model.Loja;
@@ -25,6 +26,7 @@ public class GerenciadorBaseDeDados {
 		Loja loja = new Loja("lojateste",empresa,"Loja Teste",new Date(),12);
 		Tamanho tamanho = new Tamanho("XG","Extra grande");
 		Cor cor = new Cor("AA1","Azul celeste");
+		Departamento departamento = new Departamento("DP1","Vendas");
 		
 		System.out.println("Iniciando transação.");
 		em.getTransaction().begin();
@@ -36,9 +38,12 @@ public class GerenciadorBaseDeDados {
 		System.out.println("Gravando um registro: loja");
 		em.persist(loja);
 		System.out.println("Gravando um registro: tamanho");
-		em.persist(tamanho);*/
+		em.persist(tamanho);
 		System.out.println("Gravando um registro: cor");
-		em.persist(cor);
+		em.persist(cor);*/
+		System.out.println("Gravando um registro: departamento");
+		em.persist(departamento);
+		
 				
 		System.out.println("Comitando alterações.");
 		em.getTransaction().commit();
